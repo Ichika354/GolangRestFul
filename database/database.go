da6ff8538a3f47c704f1c9239e81e7e2a9f95be0
Ichika354 <handa82624@gmail.com>
2024-06-08 15:15:28 +0700

    test deploy

--- database/database.go
+++ database/database.go
@@ -3,20 +3,40 @@ package database
 import (
     "database/sql"
     "log"
+    "os"
 
     _ "github.com/lib/pq"
+    "github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func InitDB() {
     var err error
-    connStr := "user=postgres.sxntyesqqropbxdgipqe password=UpayeW4Ok8opNViy host=aws-0-ap-southeast-1.pooler.supabase.com port=5432 dbname=postgres"
+
+    // Load environment variables from .env file
+    err = godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    // Get environment variables
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    dbname := os.Getenv("DB_NAME")
+
+    // Build connection string
+    connStr := "user=" + user + " password=" + password + " host=" + host + " port=" + port + " dbname=" + dbname
+
+    // Open connection to database
     DB, err = sql.Open("postgres", connStr)
     if err != nil {
         log.Fatal(err)
     }
 
+    // Ping database to ensure connection is established
     err = DB.Ping()
     if err != nil {
         log.Fatal(err)
@@ -24,5 +44,3 @@ func InitDB() {
 
     log.Println("Database connected")
 }
-
-// dbConnectionString := fmt.Sprintf("user=postgres.juqrvjcwczdxuyzhuqoo password=hvPFh7fEdEKXd4QN host=aws-0-ap-southeast-1.pooler.supabase.com port=5432 dbname=postgres")
--- go.mod
+++ go.mod
@@ -1,11 +1,12 @@
 module product
 
-go 1.22
+go 1.22.0
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/rs/cors v1.11.0
-	golang.org/x/crypto v0.23.0
+	golang.org/x/crypto v0.24.0
 )
